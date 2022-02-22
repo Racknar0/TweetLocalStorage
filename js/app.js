8b08a61cad18439df3032fe6706e0af57ffdd0b2
Racknar0 <racknarow1@gmail.com>
2022-02-22 00:17:52 -0500

    Timer

--- js/app.js
+++ js/app.js
@@ -1,19 +1,44 @@
-// Variables
+/************** ( '// Variables' ) **************/
 const formulario = document.querySelector('#formulario');
 const listaTeets = document.querySelector('#lista-tweets');
 let tweets = [];  //!Arreglo vacio para almacenar tweets
 
-//Event Listeners
 
+/************** ( '//Event Listeners' ) **************/
 eventListeners();
 function eventListeners() {
-    formulario.addEventListener('submit', agregarTweet)
+    formulario.addEventListener('submit', agregarTweet) //!fn de boton submit
 }
 
 
-// Functions
+
+/************** ( '// Functions' ) **************/
 function agregarTweet(e) {
     e.preventDefault();
 
-    console.log('Agregando Tweet');
+    // Text area donde se escribe
+    const tweet = document.querySelector('#tweet').value; //!fn leer value
+    
+    //validacion
+    if (tweet === ''){
+        mostarError('Un tweet no puede ir vacio');
+        return;    //! evita ejecutar mas
+    }
+
+
+}
+
+//Mostrar mensaje de error
+function mostarError(error) {
+    const mensajeError = document.createElement('P');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+    
+    //Insertarlo en el HTML
+    const contenido = document.querySelector('#contenido')
+    contenido.appendChild(mensajeError);
+
+    setTimeout(() => {
+        mensajeError.remove();kh
+    }, 3000);
 }
