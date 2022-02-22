f228e42a2941b87facb923cbe8c8a93381eadae2
Racknar0 <racknarow1@gmail.com>
2022-02-21 21:16:48 -0500

    Fn Submit

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
 // Variables
-
-
-
+const formulario = document.querySelector('#formulario');
+const listaTeets = document.querySelector('#lista-tweets');
+let tweets = [];  //!Arreglo vacio para almacenar tweets
 
 //Event Listeners
 
-
+eventListeners();
+function eventListeners() {
+    formulario.addEventListener('submit', agregarTweet)
+}
 
 
 // Functions
+function agregarTweet(e) {
+    e.preventDefault();
+
+    console.log('Agregando Tweet');
+}
